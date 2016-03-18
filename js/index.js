6b1ccf79652cb7fcae5aadbc1ba70ffbf8b17d99
kruparelia <kruparelia8416@conestogac.on.ca>
2016-03-18 12:02:22 -0400

    added google login

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var app = angular.module("myApp",["firebase"]);
+app.controller("myCtrl", function($scope,  $firebaseAuth) {
+var getRef = function(){
+        if($scope.ref == null){
+            $scope.ref = new Firebase("https://amber-inferno-359.firebaseio.com/");
+            $scope.ref.onAuth(function(authData) {
+                if (authData) {
+                    console.log("Authenticated with uid:", authData.uid);
+                    $scope.auth = authData;
+                    $scope.$apply();
+                } else {
+                    console.log("Client unauthenticated.")
+                }
+                $scope.$apply();
+            });
+        }
+        return $scope.ref;
+    }
+$scope.auth = null;
+$scope.ref = null;
+getRef();
+
+$scope.login =function() {
+        var provider = 'google';
+        var scope = {scope:'email'};
+        var auth = $firebaseAuth(getRef());
+        auth.$authWithOAuthPopup(provider, scope, function(error, authData){
+            if (error) {
+                // an error occurred while attempting login
+                alert("error: " + error);
+            }
+        });
+    };
+    $scope.logout = function(){
+        $scope.auth = null;
+        getRef().unauth();
+    }
+
+});
